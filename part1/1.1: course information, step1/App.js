d77424dd48ffccb37e95fda4620732628ac7c121
Aleksandr Kuzmich <58181433+sappho911@users.noreply.github.com>
2022-04-26 21:46:22 +0300

    1.1: course information, step1
    1.1: course information, step1

--- part1/1.1: course information, step1/App.js
+++ part1/1.1: course information, step1/App.js
@@ -9,19 +9,43 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1}: {props.exercises1}
+      </p>
+      <p>
+        {props.part2}: {props.exercises2}
+      </p>
+      <p>
+        {props.part3}: {props.exercises3}
+      </p>
+    </div>
+  )
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+};
 
 export default App
